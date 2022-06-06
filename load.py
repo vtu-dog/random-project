9fdec255f35f6dcfe52522f4327899d48fd6ccae
Wojciech Jarząbek <wojciechjarzabek@windowslive.com>
2022-06-06 10:16:18 +0200

    refine oid loading

--- load.py
+++ load.py
@@ -4,12 +4,18 @@ import os
 import json
 import pymongo
 
+from bson.objectid import ObjectId
+
 conn_str = "mongodb://root:secret@mongodb:27017/?authSource=admin&readPreference=primary&directConnection=true&ssl=false"
 client = pymongo.MongoClient(conn_str)
+client.drop_database("transactions")
 
 for file in os.listdir("/files/"):
     print("filename:", file)
     with open("/files/" + file, "r") as f:
         data = json.load(f)
+        for item in data:
+            oid = item["transaction_id"]["$oid"]
+            item["transaction_id"] = ObjectId(oid)
         client.db.transactions.insert_many(data)
         print("inserted", len(data), "records")
